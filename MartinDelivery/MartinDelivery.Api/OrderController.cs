3aee6a86f65a40d7c65a5badb4f3b27a393b6954
Hojjat Esmaeilzadeh <h.esmaeilzadeh1989@gmail.com>
2023-04-22 18:53:55 +0430

    create api project

---
+++ MartinDelivery/MartinDelivery.Api/OrderController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MartinDelivery.Api
+{
+    [Route("v1/orders")]
+    [ApiController]
+    public class OrderController : ControllerBase
+    {
+
+    }
+}
