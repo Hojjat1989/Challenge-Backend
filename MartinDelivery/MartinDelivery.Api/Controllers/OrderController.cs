45b8e52fdec5fd99ba814430d66af75d654c1183
Hojjat Esmaeilzadeh <h.esmaeilzadeh1989@gmail.com>
2023-04-23 02:18:12 +0430

    create order endpoint added

---
+++ MartinDelivery/MartinDelivery.Api/Controllers/OrderController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using MartinDelivery.Api.Models;
+using MartinDelivery.Application.Interfaces;
+using MartinDelivery.Api.Utilities;
+
+namespace MartinDelivery.Api.Controllers
+{
+    [Route("v1/orders")]
+    [ApiController]
+    public class OrderController : ControllerBase
+    {
+        private IOrderService _orderService;
+
+        public OrderController(IOrderService orderService)
+        {
+            _orderService = orderService;
+        }
+
+        [HttpPost]
+        [Route("")]
+        public IActionResult CreateOrder(CreateOrderModel order)
+        {
+            var orderDto = order.ToOrderDto();
+            _orderService.Add(orderDto);
+            return Ok();
+        }
+    }
+}
---
+++ MartinDelivery/MartinDelivery.Api/Utilities/OrderExtensions.cs
@@ -0,0 +1,27 @@
+﻿using MartinDelivery.Api.Models;
+using MartinDelivery.Application.DTOs;
+
+namespace MartinDelivery.Api.Utilities
+{
+    public static class OrderExtensions
+    {
+        public static OrderDto ToOrderDto(this CreateOrderModel model)
+        {
+            return new OrderDto
+            {
+                CreationDate = DateTime.Now,
+                OrganizationId = model.OrganizationId,
+                SenderName = model.SenderName,
+                SenderPhone = model.SenderPhone,
+                SenderAddress = model.SenderAddress,
+                SenderLatitude = model.SenderLatitude,
+                SenderLongitude = model.SenderLongitude,
+                ReceiverName = model.ReceiverName,
+                ReceiverPhone = model.ReceiverPhone,
+                ReceiverAddress = model.ReceiverAddress,
+                ReceiverLatitude = model.ReceiverLatitude,
+                ReceiverLongitude = model.ReceiverLongitude,
+            };
+        }
+    }
+}
