d026c54520b8f99cc61571d81150ea0775248c39
Hojjat Esmaeilzadeh <h.esmaeilzadeh1989@gmail.com>
2023-04-22 15:33:37 +0430

    implement infrastructure layer

---
+++ MartinDelivery/MartinDelivery.Infrastructure/MartinDeliveryDbContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Data.Entity;
+using MartinDelivery.Core.Entities;
+
+namespace MartinDelivery.Infrastructure
+{
+    public class MartinDeliveryDbContext : DbContext
+    {
+        public DbSet<Courier> Couriers { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderLog> OrderLogs { get; set; }
+        public DbSet<Organization> Organizations { get; set; }
+        public DbSet<Pack> Packs { get; set; }
+    }
+}
---
+++ MartinDelivery/MartinDelivery.Infrastructure/Repository.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Data.Entity;
+using System.Linq.Expressions;
+using MartinDelivery.Core.Base;
+
+namespace MartinDelivery.Infrastructure
+{
+    public class Repository<T> : IRepository<T> where T : EntityBase
+    {
+        private readonly MartinDeliveryDbContext _dbContext;
+
+        public Repository(MartinDeliveryDbContext context)
+        {
+            _dbContext = context;
+        }
+
+        public void Add(T entity)
+        {
+            _dbContext.Set<T>().Add(entity);
+            _dbContext.SaveChanges();
+        }
+
+        public void Update(T entity)
+        {
+            _dbContext.Entry(entity).State = EntityState.Modified;
+            _dbContext.SaveChanges();
+        }
+
+        public void Delete(T entity)
+        {
+            _dbContext.Set<T>().Remove(entity);
+            _dbContext.SaveChanges();
+        }
+
+        public T GetById(int id)
+        {
+            return _dbContext.Set<T>().FirstOrDefault(x => x.Id == id);
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            return _dbContext.Set<T>().AsEnumerable();
+        }
+
+        public IEnumerable<T> GetAll(Expression<Func<T, bool>> predicate)
+        {
+            return _dbContext.Set<T>().Where(predicate).AsEnumerable();
+        }
+    }
+}
